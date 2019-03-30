faf4dd462e3dedab6710bd2037095229dbb2de91
Eka Febryani Siskawandari <ekafebryanisiskawandari123@gmail.com>
2019-03-30 22:21:26 +0700

    Add author info and question creation date in question item

--- app/Question.php
+++ app/Question.php
@@ -9,12 +9,22 @@ class Question extends Model
     protected $fillable = ['title', 'body'];
 
     public function user() {
-      return $this->belongTo(User::class);
+      return $this->belongsTo(User::class);
     }
 
 public function setTitleAttribute($value)
 {
   $this->attributes['title'] = $value;
   $this->attributes['slug'] = str_slug($value);
+}
+public function getUrlAttribute()
+{
+  return route("questions.show", $this->id);
+  return '#';
+}
+
+public function getCreatedDateAttribute()
+{
+  return $this->created_at->diffForHumans();
 }
 }
